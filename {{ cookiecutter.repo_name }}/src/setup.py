2a969f70502c7053d50269d0295de13fee9d13c4
Robert Kasseck <robertkasseck@googlemail.com>
2022-03-14 22:14:18 +0100

    cleanup and refocator

--- {{ cookiecutter.repo_name }}/src/setup.py
+++ {{ cookiecutter.repo_name }}/src/setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name='{{ cookiecutter.repo_name }}',
-    packages=find_packages(where='src'),
-    package_dir={"": "src"},
+    packages=find_packages(where='s{{ cookiecutter.repo_name }}rc'),
+    package_dir={"": "{{ cookiecutter.repo_name }}"},
     version='0.1.0',
     description='{{ cookiecutter.description }}',
     author='{{ cookiecutter.author_name }}',
